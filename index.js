32669ce1a5bee9f43d355c4d91840fef12aa89a4
Ashley Reynolds <ashrey1110@Ashleys-MacBook-Pro.local>
2025-02-04 14:54:00 -0800

    major update

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
 
-// create variable for the submit btn and add an event listener 
-// that will call a fxn to add it to the list
-let submitBtn = document.querySelector('#submitter');
-submitBtn.addEventListener("click", (input) => addToList(input));
+//create variable to hold to-do list items
+const listItems = [];
 
 //create variable for checklist area
-let todoItem = document.querySelector('#addedToDos');
+const todoItem = document.querySelector('#currentToDos');
 
-const addToList = (inputInfo) => {
-    //add the list element to the HTML using DOM manipulation from the input
-    let newItem = document.createElement('label');
-    newItem.innerHTML = `
-    <label>
-        <input type="checkbox" id="userAddedItem"/>
-        ${inputInfo}
-    </label>
-    <br>
-    `;
+//add input to todo list
+let inputBox = document.querySelector('#toDoInput');
+
+let submitBtn = document.querySelector('#submitter');
+
+let currentInput = '';
+
+
+
+inputBox.addEventListener("input", function(event){
+    // create variable for whats in the input currently
+    currentInput = event.target.value;
+});
+
+submitBtn.addEventListener("click", (event) => {
+    //keep page from refreshing by:
+    event.preventDefault();
+    //when btn pressed, info is loaded
+    let newItem = document.createElement('div');
+    newItem.innerHTML = `${currentInput}<button class="x">x</button>`;
+    newItem.className='listItem';
     todoItem.appendChild(newItem);
-};
+    listItems.push(currentInput);
+    //clear variables
+    inputBox.value = '';
+    currentInput = '';
+});
+
+
+todoItem.addEventListener("click", (event) => {
+    //this will strikethrough an item when its clicked
+    if(event.target.className === 'listItem'){
+        event.target.className = 'striked';
+        //remove from array
+        let itemIndex = listItems.indexOf(event.target.textContent.trim());
+        listItems.splice(itemIndex, 1);
+    }
+    if(event.target.className ==='striked'){
+        event.target.className = 'listItem';
+        //add back to array
+        listItems.push(event.target.textContent);
+    }
+    if(event.target.classList.contains('x')){
+        let elem = event.target.parentNode;
+        elem.remove();
+    }
+});
+
+
