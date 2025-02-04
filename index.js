b195724783e2d392ba63f2226af6e3670b8d9ea8
Ashley Reynolds <ashrey1110@Ashleys-MacBook-Pro.local>
2025-02-04 10:09:59 -0800

    added some fxns to js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-console.log('My code is running');
 
+// create variable for the submit btn and add an event listener 
+// that will call a fxn to add it to the list
+let submitBtn = document.querySelector('#submitter');
+submitBtn.addEventListener("click", (input) => addToList(input));
+
+//create variable for checklist area
+let todoItem = document.querySelector('#addedToDos');
+
+const addToList = (inputInfo) => {
+    //add the list element to the HTML using DOM manipulation from the input
+    let newItem = document.createElement('label');
+    newItem.innerHTML = `
+    <label>
+        <input type="checkbox" id="userAddedItem"/>
+        ${inputInfo}
+    </label>
+    <br>
+    `;
+    todoItem.appendChild(newItem);
+};
